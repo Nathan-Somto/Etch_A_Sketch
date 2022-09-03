ca38db5bb54ae00b942055fe1669161a5dd8472b
Mkparu Somtochi <mkparusomtochi26@gmail.com>
2022-09-03 01:50:12 +0100

    Added the drawSketch functionality

--- js/App.js
+++ js/App.js
@@ -1,7 +1,7 @@
 const screen =document.querySelector("#screen");
 const gridSize =document.querySelectorAll("[data-number]");
-let number;
 
+let number;
 function removeDivs(){
     var child=screen.lastElementChild;
     while(child){
@@ -9,6 +9,18 @@ function removeDivs(){
         child=screen.lastElementChild;
     }
 }
+function createStartingGrid(){
+    removeDivs();
+    screen.style.gridTemplateColumns=`repeat(4,minmax(1px,1fr))`;
+    screen.style.gridTemplateRows=`repeat(4, minmax(1px,1fr))`;
+    for(let i=1; i<=16; i++){
+        let div =document.createElement("div");
+        div.className="div";
+        screen.appendChild(div);
+    } 
+}
+createStartingGrid();
+
 function setGridSize (e){
     let value =e.target.getAttribute("data-number") ;
     let numberOfDivs=Math.pow(Number(value),2);
@@ -33,18 +45,37 @@ function setGridSize (e){
         number=64;
         
     }
-    console.log(number);
     screen.style.gridTemplateColumns=`repeat(${number},minmax(1px,1fr))`;
     screen.style.gridTemplateRows=`repeat(${number}, minmax(1px,1fr))`;
     for(let i=1; i<=numberOfDivs; i++){
         let div =document.createElement("div");
         div.className="div";
+       
         
         screen.appendChild(div);
     } 
+    const divs=document.querySelectorAll(".div");
+
+divs.forEach(element=>{
+    element.addEventListener("mouseover",drawSketch);
+});
+   
 }
+
+
+const divs=document.querySelectorAll(".div");
+
+divs.forEach(element=>{
+    element.addEventListener("mouseover",drawSketch);
+})
+
+function drawSketch(e){
+    e.target.classList.add("color");
+}
+console.log(divs);
 gridSize.forEach(elements=>{
     elements.addEventListener('click',setGridSize);
-})
+});
+console.log(this);
 
 console.log(screen.textContent);
